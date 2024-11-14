ca0f8102952bc80150ec2972304a307dd240af2d
Swaroop.K.G <swaroop.k.g49@gmail.com>
2024-11-14 20:29:49 +0530

    Time: 347 ms (15.8%), Space: 48.1 MB (36.25%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,20 +1,37 @@
-import java.util.HashMap;
-
-public class Solution {
+class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        HashMap<Integer, Integer> prefixCount = new HashMap<>();
-        prefixCount.put(0, 1); 
+        int pre = 0,l = 0,r = 0,sum = 0,res = 0;
+        boolean isPre = true;
+        while(r < nums.length) {
+            if(nums[r] == 1) 
+                isPre = false;
 
-        int currentSum = 0, count = 0;
-
-        for (int num : nums) {
-            currentSum += num;
-
-            count += prefixCount.getOrDefault(currentSum - goal, 0);
-
-            prefixCount.put(currentSum, prefixCount.getOrDefault(currentSum, 0) + 1);
+            sum += nums[r];
+            while(sum > goal && l < r) {
+                pre = 0;
+                sum -= nums[l];
+                l++;
+                if(sum == goal && l < r) {
+                    while(nums[l] == 0) {
+                        pre++;
+                        l++;
+                    }
+                }
+                if(l == r) {
+                    isPre = true;
+                }
+            }
+            
+            if (sum == goal) {
+                res += pre + 1;
+            }
+            if(isPre && nums[r] == 0) {
+                pre++;
+            }
+            r++;
+            System.out.println(res);
         }
-
-        return count;
+        
+        return res;
     }
 }
