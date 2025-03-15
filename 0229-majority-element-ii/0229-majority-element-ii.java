47d86bc4fc555ab2198379561af8429ee292e006
Swaroop.K.G <swaroop.k.g49@gmail.com>
2025-03-15 23:39:51 +0530

    Time: 3 ms (71.63%), Space: 48.7 MB (6.37%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,12 +1,10 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length; //size of the array
+        int n = nums.length; 
+        int cnt1 = 0, cnt2 = 0; 
+        int el1 = Integer.MIN_VALUE;
+        int el2 = Integer.MIN_VALUE;
 
-        int cnt1 = 0, cnt2 = 0; // counts
-        int el1 = Integer.MIN_VALUE; // element 1
-        int el2 = Integer.MIN_VALUE; // element 2
-
-        // applying the Extended Boyer Moore's Voting Algorithm:
         for (int i = 0; i < n; i++) {
             if (cnt1 == 0 && el2 != nums[i]) {
                 cnt1 = 1;
