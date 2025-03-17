03e2b5a543d723b234ffbbfcd49b4d9671ca5849
Swaroop.K.G <swaroop.k.g49@gmail.com>
2025-03-17 19:11:17 +0530

    Time: 0 ms (100%), Space: 42.8 MB (65.35%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int num = 0; num < (1 << n); num++) {
+            List<Integer> sub = new ArrayList<>(); 
+
+            for (int i = 0; i < n; i++) {
+                if ((num & (1 << i)) != 0) { 
+                    sub.add(nums[i]);
+                }
+            }
+            ans.add(sub); 
+        }
+
+        return ans;
+    }
+}
