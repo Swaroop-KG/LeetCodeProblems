10378706e95ee5d84a7778c42ad4ef2f42f323ff
Swaroop.K.G <swaroop.k.g49@gmail.com>
2025-05-25 13:31:53 +0530

    Time: 28 ms (80.21%), Space: 51.5 MB (59.85%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(nums);
         
         for(int i = 0; i < n - 2; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) continue;  // skip duplicates
+            if(i > 0 && nums[i] == nums[i - 1]) continue;  
             int j = i + 1, k = n - 1;
             
             while(j < k) {
@@ -19,8 +19,8 @@ class Solution {
                     ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-                    while(j < k && nums[j] == nums[j - 1]) j++; // skip duplicates
-                    while(j < k && nums[k] == nums[k + 1]) k--; // skip duplicates
+                    while(j < k && nums[j] == nums[j - 1]) j++; 
+                    while(j < k && nums[k] == nums[k + 1]) k--; 
                 }
             }
         }
