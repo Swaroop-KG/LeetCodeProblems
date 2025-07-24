bd8b4595d226ab86bdfef07462ee273cf5a4be84
Swaroop.K.G <swaroop.k.g49@gmail.com>
2025-07-24 11:40:21 +0530

    Time: 0 ms (100%), Space: 40.4 MB (65.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int prev1=1;
         int prev2=1;
-
+        int prev1=1;
         for(int i=2;i<=n;i++){
-            int cur=prev1+prev2;
+            int cur = prev2+prev1;
             prev2=prev1;
             prev1=cur;
         }
-        return prev1;
+ return prev1;
     }
+   
 }
