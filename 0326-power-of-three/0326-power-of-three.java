97f6dcf346a94c12d4961b7e7debd868f363a64f
Swaroop.K.G <swaroop.k.g49@gmail.com>
2025-10-29 19:42:21 +0530

    Time: 7 ms (100%), Space: 44.7 MB (6.38%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,17 +1,6 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-       if(n==0 || n==2) return false;
-       else if(n==1) return true;
-       else{
-        while(n%3==0){
-            
-            n=n/3;
-        }
-            if(n==1) return true;
-            else if(n==3) return true;
-            else return false;
-        
-       }
-      
+        int maxPowerOfThree = 1162261467;
+        return n > 0 && maxPowerOfThree % n == 0;
     }
 }
