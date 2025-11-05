c9da6733d59b1912f92c85368babba05de189246
Swaroop.K.G <swaroop.k.g49@gmail.com>
2025-11-05 21:38:26 +0530

    Time: 1 ms (65.51%), Space: 41 MB (32.14%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean isHappy(int n) {
-        
+        Set<Integer> usedInteger = new HashSet<>();
+        while (n != 1 && !usedInteger.contains(n)) {
+            usedInteger.add(n); 
+            int sum = 0;
+            while (n != 0) {
+                sum += (n % 10) * (n % 10); 
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1; 
     }
 }
